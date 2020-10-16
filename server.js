93196194ce31895bfee38b0037549f2545e37dcd
Bing Z <bing@bings-mbp.lan>
2020-10-16 15:14:06 -0600

    set app dir to static public dir

--- server.js
+++ server.js
@@ -2,9 +2,17 @@
  ============================================================================================== */
 
 const express = require('express');
+
+const connection = require('./config/connection')
+
 const app = express(); 
 
 const PORT = process.env.PORT || 3000;
+
+//Serve static content for the app from the 'Public' directory in the app directory 
+app.use(express.static("public"));
+
+
 
 
 
