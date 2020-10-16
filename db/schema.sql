20aa7df87c9decfff858b652863e1f18feb2d65c
Bing Z <bing@bings-mbp.lan>
2020-10-16 14:30:59 -0600

    created database n table

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- CREATE DATABASE
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+
+-- CREATE TABLE
+DROP TABLE IF EXISTS burgers;
+CREATE TABLE burgers(
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(35) NOT NULL,
+  devoured BOOLEAN,
+  PRIMARY KEY(id)
+);
