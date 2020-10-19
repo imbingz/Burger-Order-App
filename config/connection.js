093045d65b9cd83737a2e47d582dfcab7c03bc3b
Bing Z <bing@bings-mbp.lan>
2020-10-19 13:44:26 -0600

    update connection for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,23 @@
 const mysql = require("mysql");
 require('dotenv').config(); 
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.MYSQLPASSWORD,
-  database: "burgers_db"
-});
+let connection;
+
+//Check if JASWDB for database to use
+
+if (process.env.JAWSDB_URL) {
+  //use jaws_db
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //use local connection
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQLPASSWORD,
+    database: "burgers_db"
+  });
+}; 
 
 // Make connection
 connection.connect(function(err) {
