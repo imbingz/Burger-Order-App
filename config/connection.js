5d7a4ba9b9a2690f4d732107d961c89182b73fcb
Bing Z <bing@bings-mbp.lan>
2020-10-19 13:31:25 -0600

    update connection using dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 // Set up MySQL connection
 const mysql = require("mysql");
+require('dotenv').config(); 
 
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
+  password: process.env.MYSQLPASSWORD,
   database: "burgers_db"
 });
 
