a150caf279c93defa32e9ccf1c11f9da5883dc74
Bing Z <bing@bings-mbp.lan>
2020-10-19 13:33:23 -0600

    updated seeds with less data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,15 @@
 -- ADD VALUES TO TABLE 
 
 INSERT INTO burgers (burger_name, devoured) VALUES 
-  ('Cheezy Baby', 1),
-  ('Big Meatloaf', 0),
-  ('Double Stacks', 0),
-  ('Mushroom Paradize', 1),
-  ('Spicy Tofu', 1),
-  ('Garden Veggie', 1),
-  ('Beanlicious', 0);
+  ('Meatless Rainbow', 0),
+  ('Tasty Vegan', 0),
+  ('Egg on Top', 0),
+  ('Cheezy Baby', 0);
+  -- ('Big Meatloaf', 0),
+  -- ('Double Stacks', 0),
+  -- ('Mushroom Paradize', 1),
+  -- ('Spicy Tofu', 1),
+  -- ('Garden Veggie', 1),
+  -- ('Beanlicious', 0);
+
 
