c52b702216b1177d3f2782d810f398cd796cf879
Bing Z <bing@bings-mbp.lan>
2020-11-06 09:56:04 -0700

    changed to arrow function

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,43 +1,42 @@
 //Wait for DOM is fully loaded before attaching controllers
 
 $(function() {
-  //send PUT request upon clicking devour-button, the burger-img
-  $(".devour-btn").on('click', function() {
+	//send PUT request upon clicking devour-button, the burger-img
+	$(".devour-btn").on("click", function() {
+		let id = $(this).data("id");
+		let devourState = {
+			devoured: "true",
+		};
 
-    let id = $(this).data("id");
-    let devourState = {
-      devoured: "true"
-    };
+		$.ajax("api/burgers/" + id, {
+			type: "PUT",
+			data: devourState,
+		}).then(() => {
+			//reload the page to get the updated list
+			location.reload();
+		});
+	});
 
-    $.ajax("api/burgers/" + id, {
-      type: "PUT",
-      data: devourState
-    }).then(function() {
-      //reload the page to get the updated list 
-      location.reload();
-    });
-  });
+	//Send POST request
+	$(".order-form").on("submit", function(event) {
+		//Prevent page reload on a submit event
+		event.preventDefault();
 
-  //Send POST request 
-  $(".order-form").on('submit', function(event) {
-    //Prevent page reload on a submit event
-    event.preventDefault();
+		let newBurger;
 
-    let newBurger;
+		if ($("textarea").val()) {
+			newBurger = {
+				burger_name: $("textarea").val().trim(),
+				devoured: 0,
+			};
+		}
 
-    if ($('textarea').val()) {
-      newBurger = {
-        burger_name: $("textarea").val().trim(),
-        devoured: 0
-      };
-    }
-
-    $.ajax('/api/burgers', {
-      type: "POST",
-      data: newBurger
-    }).then(() => {
-      //Reload page to get the updated list
-      location.reload();
-    });
-  });
+		$.ajax("/api/burgers", {
+			type: "POST",
+			data: newBurger,
+		}).then(() => {
+			//Reload page to get the updated list
+			location.reload();
+		});
+	});
 });
--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 /* IMPORT MODULES AND SETUP SERVER
  ============================================================================================== */
 
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-const connection = require('./config/connection');
-const routes = require('./controllers/burger_controller')
+const connection = require("./config/connection");
+const routes = require("./controllers/burger_controller");
 
-const app = express(); 
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-//Serve static content for the app from the 'Public' directory in the app directory 
+//Serve static content for the app from the 'Public' directory in the app directory
 app.use(express.static("public"));
 
 //Middleware to parse application body
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
 
 //Handglebars setup
-app.engine('handlebars', exphbs({ defaultLayout: "main" }));
-app.set('view engine', "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //Use routes
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log('Server listening on http://localhost:' + PORT);
-})
+	console.log("Server listening on http://localhost:" + PORT);
+});
